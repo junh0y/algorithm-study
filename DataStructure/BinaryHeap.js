3e1b1e511841157f146661328858ea609c1f1543
junh0y <y.junh0y@gmail.com>
2026-02-24 15:10:53 +0900

    Priority Queue

--- DataStructure/BinaryHeap.js
+++ DataStructure/BinaryHeap.js
@@ -148,3 +148,130 @@ heap.extractMax();
 console.log(heap);
 heap.extractMax();
 console.log(heap);
+
+/*
+  What is a Priority Queue?
+  : A data stucture where each element has a priority.
+    Elements with higher priorities are served before elements with lower priorities.
+
+  · Wirte a Min Binary Heap - lower number means higher priority.
+  · Each Node has a value and a priority. Use the priority to build the heap.
+  · Enqueue method accepts a value and priority,
+    makes a new node, and puts it in the right spot based off of its priority.
+  · Dequeue method removes root element,
+    returns it, and rearranges heap using priority.
+*/
+
+class PriorityQueue {
+  constructor() {
+    this.values = [];
+  }
+
+  enqueue(value, priority) {
+    let newNode = new Node(value, priority);
+    this.values.push(newNode);
+    this.bubbleUp();
+  }
+  bubbleUp() {
+    let idx = this.values.length - 1;
+    const element = this.values[idx];
+    while (idx > 0) {
+      let parentIdx = Math.floor((idx - 1) / 2);
+      let parent = this.values[parentIdx];
+
+      if (element > parent) {
+        this.values[parentIdx] = element;
+        this.values[idx] = parent;
+        idx = parentIdx;
+      } else {
+        break;
+      }
+    }
+  }
+  dequeue() {
+    const max = this.values[0];
+    const end = this.values.pop();
+
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
+
+    return max;
+  }
+  sinkDown() {
+    let idx = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+
+    while (true) {
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      let leftChild;
+      let rightChild;
+      let swap = null;
+
+      if (leftChildIdx < length) {
+        leftChild = this.values[leftChildIdx];
+        if (leftChild.priority > element.priority) {
+          swap = leftChildIdx;
+        }
+      }
+
+      if (rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx];
+        if (
+          (swap === null && rightChild.priority > element.priority) ||
+          (swap !== null && rightChild.priority > leftChild.priority)
+        ) {
+          swap = rightChildIdx;
+        }
+      }
+      
+      if (swap === null) break;
+
+      this.values[idx] = this.values[swap];
+      this.values[swap] = element;
+      idx = swap;
+    }
+  }
+}
+
+class Node {
+  constructor(value, priority) {
+    this.value = value;
+    this.priority = priority;
+  }
+}
+
+let ER = new PriorityQueue();
+ER.enqueue('common cold', 5);
+ER.enqueue('gunshot wound', 1);
+ER.enqueue('high fever', 4);
+ER.enqueue('broken arm', 2);
+ER.enqueue('glass in foot', 3);
+console.log(ER);
+console.log(ER.dequeue());
+console.log(ER);
+console.log(ER.dequeue());
+console.log(ER);
+console.log(ER.dequeue());
+console.log(ER);
+console.log(ER.dequeue());
+console.log(ER);
+
+/*
+  Big O of Binary Heaps
+
+  Insetion  - O(logN)
+  Removal   - O(logN)
+  Search    - O(N)
+
+
+  Recap
+  · Binary Heaps are very useful data structures for sotring,
+    and implementing other data structures like priority queues.
+  · Binary Heaps are either MaxBinaryHeaps or MinBinaryHeaps
+    with parents either being smaller or larger than their children.
+  · With just a little bit of math, we can represent heaps using arrays.
+*/
